f6483a8de39882f10aef7e383a097b906c2bbab2
TOKITA, Hiroshi <tokita.hiroshi@gmail.com>
2020-11-07 10:06:12 +0900

    debug

--- napi/lib/iotivity-lite.js
+++ napi/lib/iotivity-lite.js
@@ -1,4 +1,4 @@
 var path = '../build/Release/';
-if (process.env.IOTIVITY_LITE_DEBUG == '1') { path = '../build/Debug/'; }
+if (process.env.IOTIVITY_LITE_NODE_DEBUG == '1') { path = '../build/Debug/'; }
 const addon = require(path + 'iotivity-lite-native');
 module.exports = addon;
